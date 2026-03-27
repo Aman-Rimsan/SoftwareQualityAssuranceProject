2fcc0c708eb7648ca87fc591fd8473ca3ffb736d
AbbasAkbar7 <90078145+AbbasAkbar7@users.noreply.github.com>
2026-03-27 13:51:54 -0400

    Update test_backend.py

--- back-end/test_backend.py
+++ back-end/test_backend.py
@@ -22,29 +22,5 @@ class TestReadTransactions(unittest.TestCase):
         self.assertTrue(isinstance(transactions, list))
 
 
-# METHOD 2 TESTS
-class TestBackendRun(unittest.TestCase):
-
-    def test_run_no_transactions(self):
-        backend = BackEndSystem("old_master.txt","empty.txt","new_master.txt","new_current.txt")
-        backend.run()
-        self.assertTrue(os.path.exists("new_master.txt"))
-
-    def test_run_single_transaction(self):
-        backend = BackEndSystem("old_master.txt","one_transaction.txt","new_master.txt","new_current.txt")
-        backend.run()
-        self.assertTrue(os.path.exists("new_master.txt"))
-
-    def test_run_multiple_transactions(self):
-        backend = BackEndSystem("old_master.txt","many_transactions.txt","new_master.txt","new_current.txt")
-        backend.run()
-        self.assertTrue(os.path.exists("new_master.txt"))
-
-    def test_invalid_transaction(self):
-        backend = BackEndSystem("old_master.txt","invalid_transaction.txt","new_master.txt","new_current.txt")
-        backend.run()
-        self.assertTrue(os.path.exists("new_master.txt"))
-
-
 if __name__ == "__main__":
     unittest.main()
